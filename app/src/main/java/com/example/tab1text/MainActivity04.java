f1bbba590feac22944ee3147bb8561cb7e32d14d
HengYiZhao <zhy.ai@qq.com>
2017-01-10 16:25:32 +0800

    添加gif 文件

--- app/src/main/java/com/example/tab1text/MainActivity04.java
+++ app/src/main/java/com/example/tab1text/MainActivity04.java
@@ -3,8 +3,6 @@ package com.example.tab1text;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.view.View;
-
 import com.viewpagerindicator.TabPageIndicator;
 
 /**
